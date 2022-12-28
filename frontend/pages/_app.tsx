324ca98be5988ca1ed31a77ea68729625662da85
Leopold-V <leopoldV79@gmail.com>
2022-12-28 10:44:43 +0100

    preparing for first vercel deployment

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { SessionProvider  } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
+//@ts-ignore
 import FidbackHub from "fidbackhub";
 import '../styles/globals.css'
 import Layout from "components/layout";
